df4f4ccf63838b1d9156ae1aa4f31caa0380aa26
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2020-10-27 16:44:33 +0330

    Add the Download Function to the program

---
+++ Cross Download Manager/Download/DownloadUrl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace CrossDownloadManager.Download
+{
+    public class DownloadUrl
+    {
+        public static void DownloadFromUrl()
+        {
+            // Initialize the WebClient
+            var webClient = new WebClient();
+            Console.Write("Download Link: ");
+            // Receive the Url
+            string fileUrl = Console.ReadLine();
+            // Separating the file name
+            string fileName = DownloadUrl.getFileName(fileUrl);
+            webClient.DownloadFile(fileUrl, fileName);
+            Console.WriteLine("Download Completed");
+        }
+
+        // Separating the file name
+        private static string getFileName(string fileUrl)
+        {
+            string[] urlArray = fileUrl.Split("/");
+            string fileName = urlArray[urlArray.Length - 1];
+            return fileName;
+        }
+    }
+}
--- Cross Download Manager/Program.cs
+++ Cross Download Manager/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using CrossDownloadManager.Download;
 
-namespace Cross_Download_Manager
+namespace CrossDownloadManager
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DownloadUrl.DownloadFromUrl();
         }
     }
 }
