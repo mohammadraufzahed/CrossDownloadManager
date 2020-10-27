3cce4a34e956af198c7376d1d3f3f0bfbd74af5a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2020-10-27 16:00:17 +0330

    Initial the project

---
+++ Cross Download Manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cross_Download_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
